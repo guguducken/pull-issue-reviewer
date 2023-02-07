7d6ec578e74b90ec887741f5205d4f7e60f6bf4e
guguducken <1173781832@qq.com>
2023-02-07 19:40:59 +0800

    change repo to test

--- src/index.js
+++ src/index.js
@@ -41,6 +41,9 @@ async function run() {
         for (let i = 0; i < prs.length; i++) {
             const pr = prs[i];
             core.info(`Start to check pull ${pr.number}, title: ${pr.title} >>>>>>`);
+            if (pr.body === null) {
+                core.info(`There is no body in is pr ${pr.number}..... skip`);
+            }
             let num_issues = getReleatedIssueNumber(pr.body);
 
             if (num_issues.length != 0) {
